b88728a270194b862121a7b551127b1de76e59ee
adriel <adrieldev@outlook.com>
2023-01-17 08:23:42 -0300

    crud

---
+++ BuberBreakfast/BuberBreakfast.Contracts/Breakfast/BreakfastResponse.cs
@@ -0,0 +1,13 @@
+namespace BuberBreakfast.Contracts.Breakfast;
+using System;
+using System.Collections.Generic;
+
+public record BreakfastResponse(
+    Guid Id,
+    string Name,
+    string description,
+    DateTime StartDateTime,
+    DateTime EndDateTime,
+    DateTime LastModifiedDateTime,
+    List<string> Savory,
+    List<string> Sweet);
---
+++ BuberBreakfast/BuberBreakfast.Contracts/Breakfast/UpsertBreakfastRequest.cs
@@ -0,0 +1,12 @@
+namespace BuberBreakfast.Contracts.Breakfast;
+using System;
+using System.Collections.Generic;
+
+public record UpsertBreakfastRequest(
+
+    string Name,
+    string description,
+    DateTime StartDateTime,
+    DateTime EndDateTime,
+    List<string> Savory,
+    List<string> Sweet);
